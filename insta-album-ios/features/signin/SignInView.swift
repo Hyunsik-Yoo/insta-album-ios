95066f0f98668763df28520897747578b295511b
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-05-29 10:31:11 +0900

    첫화면에 애니메이션 적용

--- insta-album-ios/features/signin/SignInView.swift
+++ insta-album-ios/features/signin/SignInView.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Then
 import SnapKit
+import Foundation
 
 class SignInView: BaseView {
     let continueBtn = UIButton().then {
@@ -8,12 +9,23 @@ class SignInView: BaseView {
         $0.setTitle("인스타로 계속하기", for: .normal)
     }
     
+    let bgImage = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.image = UIImage(named: "onboard1")
+        $0.alpha = 0.6
+    }
+    
     override func setup() {
         backgroundColor = .white
-        addSubViews(continueBtn)
+        addSubViews(bgImage, continueBtn)
     }
     
     override func bindConstraints() {
+        
+        bgImage.snp.makeConstraints { (make) in
+            make.edges.equalTo(0)
+        }
+        
         continueBtn.snp.makeConstraints { (make) in
             make.center.equalToSuperview()
             make.left.equalToSuperview().offset(20)
